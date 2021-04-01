74598b2d459fd845adb3d188841393ca73f3ed0e
Garnier <corentingarnier06@gmail.com>
2021-04-01 16:27:14 +0200

    Initialisation du projet
    Set-up du front sous vue et setup de l'api avec connexion a notre base de donnees sous mongoDB

---
+++ API/index.js
@@ -0,0 +1,41 @@
+const {MongoClient} = require('mongodb');
+const express = require('express');
+const app = express();
+const port = 8090;
+
+async function main(){
+    /**
+     * Connection URI. Update <username>, <password>, and <your-cluster-url> to reflect your cluster.
+     * See https://docs.mongodb.com/ecosystem/drivers/node/ for more details
+     */
+    const uri = "mongodb+srv://Garmin-admin:nanspd06@projet-garmin.k2tj4.mongodb.net/Projet-Garmin?retryWrites=true&w=majority";
+ 
+
+    const client = new MongoClient(uri);
+ 
+    try {
+        // Connect to the MongoDB cluster
+        await client.connect();
+ 
+        // Make the appropriate DB calls
+        await  listDatabases(client);
+ 
+    } catch (e) {
+        console.error(e);
+    } finally {
+        await client.close();
+    }
+}
+
+async function listDatabases(client){
+    databasesList = await client.db().admin().listDatabases();
+ 
+    console.log("Databases:");
+    databasesList.databases.forEach(db => console.log(` - ${db.name}`));
+};
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+});
+
+main().catch(console.error);
